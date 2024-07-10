038b5d2c6ada00c2712a9a232106ed75ad21e5f1
noahhusby <noahhusby@gmail.com>
2024-07-09 22:22:43 -0400

    feat: add controller variable support

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-        name='russound_rio',
+        name='aiorussound',
         version='1.0.0',
-        packages=['russound_rio'],
+        packages=['aiorussound'],
         license='MIT',
-        author='Chris Phillips',
-        url='https://github.com/chphilli/russound_rio',
+        author='Noah Husby',
+        url='https://github.com/noahhusby/russound_rio',
         description='Asyncio client for Russound RIO devices',
-        long_description=open('README.rst').read(),
+        long_description=open('README.md').read(),
 )
