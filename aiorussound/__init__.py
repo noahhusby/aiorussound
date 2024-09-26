847d6ce8f47f24efac4a75a6da5ae1a91150953b
archiconda1976 <82009380+archiconda1976@users.noreply.github.com>
2024-09-26 10:40:56 -0500

    Clean-up misc. whitespace deltas

--- aiorussound/__init__.py
+++ aiorussound/__init__.py
@@ -13,7 +13,6 @@ from .connection import RussoundTcpConnectionHandler
 from .models import Source, RussoundMessage, Zone, Favorite
 from .rio import Controller, RussoundClient
 
-
 __all__ = [
     "RussoundError",
     "CommandError",
