6581845b8d46d2f456bbb335a230b98a36437085
noahhusby <32528627+noahhusby@users.noreply.github.com>
2025-07-03 09:31:06 -0500

    Add serial connection handler

--- aiorussound/connection.py
+++ aiorussound/connection.py
@@ -4,8 +4,11 @@ from abc import abstractmethod
 from asyncio import StreamReader
 from typing import Optional
 
+import serial_asyncio_fast
+
 from aiorussound.const import (
     DEFAULT_PORT,
+    DEFAULT_BAUDRATE,
     TIMEOUT,
 )
 
@@ -43,6 +46,29 @@ class RussoundTcpConnectionHandler(RussoundConnectionHandler):
             reader, writer = await asyncio.open_connection(self.host, self.port)
         self.reader = reader
         self.writer = writer
+
+    async def send(self, cmd: str) -> None:
+        """Send a command to the Russound client."""
+        await super().send(cmd)
+        self.writer.write(bytearray(f"{cmd}\r", "utf-8"))
+        await self.writer.drain()
+
+
+class RussoundSerialConnectionHandler(RussoundConnectionHandler):
+    def __init__(self, port: str, baudrate: int = DEFAULT_BAUDRATE) -> None:
+        """Initialize the Russound object using the event loop, port and baudrate provided."""
+        super().__init__()
+        self.port = port
+        self.baudrate = baudrate
+        self.writer = None
+
+    async def connect(self) -> None:
+        _LOGGER.debug("Connecting to %s (baudrate: %s)", self.port, self.baudrate)
+        async with asyncio.timeout(TIMEOUT):
+            self.reader, self.writer = await serial_asyncio_fast.open_serial_connection(
+                url=self.port,
+                baudrate=self.baudrate,
+            )
 
     async def send(self, cmd: str) -> None:
         """Send a command to the Russound client."""
